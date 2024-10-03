cd4ab1776d1dae0d7579080c606ef7b27e6e7dff
Angus <>
2024-10-03 12:50:32 +0800

    modifiy zh_TW to zh-TW

--- docs/CROWDIN/zh-TW/conf.py
+++ docs/CROWDIN/zh-TW/conf.py
@@ -1,3 +1,10 @@
 import os
 
 exec (open("../../shared.conf.py").read())
+
+# 指定語言代碼
+language = 'zh-TW'
+
+# 語言檔案路徑
+locale_dirs = ['locale/']  # 你的翻譯檔案所在的目錄
+gettext_compact = False    # 確保每個文件都有單獨的翻譯檔案
