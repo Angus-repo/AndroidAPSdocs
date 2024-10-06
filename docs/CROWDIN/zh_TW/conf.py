1b88e700a2dbc7c09fcdd6f4b445be3d63fb264d
Angus <lh0528@gmail.com>
2024-10-06 17:53:51 +0800

    change zh-TW to zh_TW

--- docs/CROWDIN/zh_TW/conf.py
+++ docs/CROWDIN/zh_TW/conf.py
@@ -6,5 +6,5 @@ exec (open("../../shared.conf.py").read())
 language = 'zh-TW'
 
 # 語言檔案路徑
-locale_dirs = ['locale/']  # 你的翻譯檔案所在的目錄
+locale_dirs = ['docs/CROWDIN/']  # 你的翻譯檔案所在的目錄
 gettext_compact = False    # 確保每個文件都有單獨的翻譯檔案
